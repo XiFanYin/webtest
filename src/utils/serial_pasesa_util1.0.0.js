ed8fd6d083a6bfb256d2cfc71116ae91daa1dc11
XiFanYin <807142217@qq.com>
2021-09-17 18:01:11 +0800

    优化工具类

--- src/utils/serial_pasesa_util1.0.0.js
+++ src/utils/serial_pasesa_util1.0.0.js
@@ -31,7 +31,7 @@ class SingletonPasesaSerial {
                 CONNECTING: 1, //连接中
                 IDLE: 2, //闲置
                 WORK: 3, //工作
-                STOPWORKING: 4//正在停止中
+                STOPWORKING: 4 //正在停止中
             };
 
             //错误码定义
@@ -48,13 +48,10 @@ class SingletonPasesaSerial {
                     code: -2,
                     message: "连接失败"
                 },
-                REPEATCONNECT: {
-                    code: -3,
-                    message: "已连接，无需重复连接"
-                },
-                FRISTCONNECT: {
+
+                ISCONNECTING: {
                     code: -4,
-                    message: "请先连接串口"
+                    message: "正在连接中"
                 },
                 BPBUSY: {
                     code: -5,
@@ -90,8 +87,8 @@ class SingletonPasesaSerial {
             let mErrorListener = null;
             //测量结果回调
             let mResultListener = null;
-            var sex = 0
-            var age = 0x12
+            var mSex = 0
+            var mAge = 0x12
             //数据拼接
             var resultData = [];
             //定时器
@@ -128,13 +125,17 @@ class SingletonPasesaSerial {
             /**
              * 打开串口，同时发送connect命令
              */
-            this.connectSerial = async function () {
+            this.startMeasure = async function (age, sex) {
+                mAge = age
+                mSex = sex
                 if (currentState == CONNECTSTATE.UNCONNECT) {
                     //改变链接状态为连接中
                     changeState(CONNECTSTATE.CONNECTING)
                     try {
                         //获取串口对象
-                        port = await navigator.serial.requestPort({ filters });
+                        port = await navigator.serial.requestPort({
+                            filters
+                        });
                         //打开串口
                         await port.open({
                             baudRate: 115200,
@@ -160,16 +161,20 @@ class SingletonPasesaSerial {
                         }
 
                     }
-                } else {
-                    //如果连接中
-                    sendConnectCmd()
+                } else if (currentState == CONNECTSTATE.CONNECTING) {
+                    callError(ERRORSTATE.ISCONNECTING)
+                } else if (currentState == CONNECTSTATE.IDLE) {
+                    //如果是闲置，就调用测量命令
+                    start()
+                } else if (currentState == CONNECTSTATE.WORK || currentState == CONNECTSTATE.STOPWORKING) {
+                    callError(ERRORSTATE.BPBUSY)
                 }
             }
 
 
             /**
-           * 监听串口回执数据
-           */
+             * 监听串口回执数据
+             */
             async function readListener() {
                 while (port.readable && keepReading) {
                     reader = port.readable.getReader();
@@ -191,29 +196,30 @@ class SingletonPasesaSerial {
                                 //数据做拼接
                                 resultData = resultData.concat(tempdata)
                                 //机器按下了开始键--过滤掉53命令
-                                if(resultData.length == 6&&resultData[1]=="53"){
+                                if (resultData.length == 6 && resultData[1] == "53") {
                                     resultData.length = 0;
                                 }
                                 //长度等于7.表示是命令的回执
                                 if (resultData.length == 7) {
                                     switch (resultData[1]) {
-                                        case "01": {
+                                        case "01":
                                             //改变设备连接状态
                                             changeState(CONNECTSTATE.IDLE)
                                             //清空数据
                                             resultData.length = 0;
+                                            //如果是闲置，就调用测量命令
+                                            start()
                                             break
-                                        }
-                                        case "02": {
+                                        case "02":
                                             //改变设备连接状态
                                             changeState(CONNECTSTATE.UNCONNECT)
                                             //清空数据
                                             resultData.length = 0;
                                             keepReading = false;
                                             reader.cancel();
-                                        }
 
-                                        case "51": {
+                                            break
+                                        case "51":
                                             if (resultData[5] == "00" || resultData[5] == "01") {
                                                 //写入测量命令
                                                 writeCommand(new Uint8Array(MEASURE))
@@ -226,8 +232,8 @@ class SingletonPasesaSerial {
                                                 callError(ERRORSTATE.MEASUREFAIL)
                                             }
                                             break
-                                        }
-                                        case "52": {
+
+                                        case "52":
                                             if (resultData[5] == "00") {
                                                 changeState(CONNECTSTATE.WORK)
                                                 //清空数据
@@ -238,7 +244,7 @@ class SingletonPasesaSerial {
                                                 resultData.length = 0
                                             }
                                             break
-                                        }
+
                                     }
 
                                 }
@@ -300,8 +306,8 @@ class SingletonPasesaSerial {
             }
 
             /**
-            * 数据恢复初始值
-            */
+             * 数据恢复初始值
+             */
             async function resetData() {
                 try {
                     await port.close();
@@ -338,39 +344,24 @@ class SingletonPasesaSerial {
                     }, 1000);
                     //写入连接命令
                     writeCommand(CONNECT)
-                } else if (currentState == CONNECTSTATE.IDLE || currentState == CONNECTSTATE.WORK || currentState == CONNECTSTATE.STOPWORKING) {
-                    //如果正在测量，再次点击测量
-                    callError(ERRORSTATE.REPEATCONNECT);
                 }
             }
 
             /**
-             * 开始测量，首先去看看设备处于什么状态
+             * 开始测量
              */
-            this.startMeasure = function (age, sex) {
-                this.age = age
-                this.sex = sex
-                //请求确认状态
-                if (currentState == CONNECTSTATE.IDLE) {
-                    //重置测量命令
-                    MEASURE = [0x40, 0x52, 0x06, 0x00, 0xFF, 0x00];
-                    MEASURE.push(age)
-                    MEASURE.push(sex)
-                    MEASURE.push(0x00)
-                    //转换开始命令
-                    MEASURE = getCMD(MEASURE)
-                    //请求状态
-                    writeCommand(STATE)
-                } else {
-                    //非闲置状态爆出对应错误
-                    if (currentState == CONNECTSTATE.UNCONNECT) {
-                        callError(ERRORSTATE.FRISTCONNECT)
-                    } else {
-                        callError(ERRORSTATE.BPBUSY)
-                    }
-
-                }
+            function start() {
+                //重置测量命令
+                MEASURE = [0x40, 0x52, 0x06, 0x00, 0xFF, 0x00];
+                MEASURE.push(mAge)
+                MEASURE.push(mSex)
+                MEASURE.push(0x00)
+                //转换开始命令
+                MEASURE = getCMD(MEASURE)
+                //请求状态
+                writeCommand(STATE)
             }
+
             /**
              * 
              * 停止测量
@@ -378,8 +369,8 @@ class SingletonPasesaSerial {
             this.stopMeasure = function () {
                 if (currentState == CONNECTSTATE.WORK) {
                     STOPMEASURE = [0x40, 0x52, 0x06, 0x00, 0xFF, 0xFF];
-                    STOPMEASURE.push(age)
-                    STOPMEASURE.push(sex)
+                    STOPMEASURE.push(mAge)
+                    STOPMEASURE.push(mSex)
                     STOPMEASURE.push(0x00)
                     //转换开始命令
                     STOPMEASURE = getCMD(STOPMEASURE)
@@ -392,9 +383,8 @@ class SingletonPasesaSerial {
             }
 
             this.disconnectSerial = async function () {
-
                 //只有连接了才能断开连接
-                if (currentState != CONNECTSTATE.UNCONNECT) {
+                if (currentState == CONNECTSTATE.IDLE || currentState == CONNECTSTATE.WORK || currentState == CONNECTSTATE.STOPWORKING) {
                     //关闭串口时候，如果正在测量，就停止测量
                     if (currentState == CONNECTSTATE.WORK) {
                         this.stopMeasure()
@@ -402,26 +392,23 @@ class SingletonPasesaSerial {
                     setTimeout(() => {
                         writeCommand(DISCONNECT)
                     }, 500);
-
-                } else {
-                    callError(ERRORSTATE.FRISTCONNECT)
                 }
-
-
             }
 
             /**
-            * 
-            * 串口写入命令
-            */
+             * 
+             * 串口写入命令
+             */
             async function writeCommand(command) {
-                await writer.write(command);
+                if(writer){
+                    await writer.write(command);
+                }
             };
 
             /**
-            * 
-            * 改变当前连接状态，对外部进行回调
-            */
+             * 
+             * 改变当前连接状态，对外部进行回调
+             */
             function changeState(state) {
                 currentState = state;
                 //向外回调当前状态
@@ -432,8 +419,8 @@ class SingletonPasesaSerial {
             }
 
             /**
-            * 错误回调
-            */
+             * 错误回调
+             */
             function callError(errormessage) {
                 if (mErrorListener) {
                     mErrorListener(errormessage);
